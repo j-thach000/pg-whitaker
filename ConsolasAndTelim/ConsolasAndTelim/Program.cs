64927adb6dbd43a81964e833bb53ca0edaa15d52
James Thach <j.thach000@gmail.com>
2022-11-25 17:42:49 -0500

    Add completed challenge

---
+++ ConsolasAndTelim/ConsolasAndTelim/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread.");

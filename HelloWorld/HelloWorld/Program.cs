6c332c952fdc851fc9d5b66d94d52ef710f1c348
James Thach <j.thach000@gmail.com>
2022-11-25 17:43:23 -0500

    Modify with scratch code snippet

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!\n");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The caster flings a fireball in your direction.");
+Console.WriteLine("Time begins to slow to a crawl.");
+Console.WriteLine("You're able to dodge the fireball effortlessly.");
+Console.WriteLine("You fire a retaliatory fireball back at the enemy caster.");
+
+string name;
+name = "User";
+Console.WriteLine("Hi " + name);
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+Console.WriteLine("Hi " + name);
+

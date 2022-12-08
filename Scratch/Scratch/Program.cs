ca7871b9e1b5745499e2600895d8c5ca2afabe4d
James Thach <j.thach000@gmail.com>
2022-12-08 16:45:39 -0500

    Challenge done again

--- Scratch/Scratch/Program.cs
+++ Scratch/Scratch/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string bread_recipient;
+bread_recipient = Console.ReadLine();
+Console.WriteLine("Noted: " + bread_recipient + " got bread.");

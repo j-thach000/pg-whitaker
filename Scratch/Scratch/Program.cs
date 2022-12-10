33957b3fef6ba2dcf7228ec0265bc44f8c37a422
James Thach <j.thach000@gmail.com>
2022-12-10 05:46:08 -0500

    Add completed chapter snippets

--- Scratch/Scratch/Program.cs
+++ Scratch/Scratch/Program.cs
@@ -1,7 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Bread is ready.");
-Console.WriteLine("Who is the bread for?");
-string bread_recipient;
-bread_recipient = Console.ReadLine();
-Console.WriteLine("Noted: " + bread_recipient + " got bread.");
+/* 12/09/22
+ // initial declaration and assignment of username
+Console.WriteLine("Input username: ");
+string username; // declaring a variable
+username = Console.ReadLine(); // assigning a value to a variable
+Console.WriteLine("Hi " + username);
+
+// assigning username another value
+Console.WriteLine("Input another username: ");
+username = Console.ReadLine();
+Console.WriteLine("Hi " + username);
+
+// assigning username another value
+Console.WriteLine("Input favorite color: ");
+string favoriteColor;
+favoriteColor = Console.ReadLine();
+Console.WriteLine("Your favorite color is " + favoriteColor);
+*/
+
+// int data types
+int score;
+score = 4;
+Console.WriteLine(score);
+score = 11;
+Console.WriteLine(score);
+score = -1564;
+Console.WriteLine(score);
+// wish i knew how to declare some data structure to hold all these ints atm
+
+int a, b, c;
+a = b = c = 10;
+Console.WriteLine(a + b + c);

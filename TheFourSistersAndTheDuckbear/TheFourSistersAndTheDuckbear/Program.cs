3d6cfe3f06b6b82643821433b1246f2f2eefcbe9
James Thach <j.thach000@gmail.com>
2022-12-13 15:31:19 -0500

    Add completed challenge

---
+++ TheFourSistersAndTheDuckbear/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Todo:  
+ * accept user input for integers
+ * store and print the amount of eggs for the sisters
+ * store and print the amount of eggs for the duckbear
+ */
+
+Console.WriteLine("Enter the number of eggs gathered today: ");
+string eggsText = Console.ReadLine();
+int eggs = Convert.ToInt16(eggsText);
+int eggsReceivedBySisters = eggs / 4; // 4 sisters described in challenge
+int eggsReceivedByDuckbear = eggs % 4;
+Console.WriteLine("Eggs received by sisters: " + eggsReceivedBySisters);
+Console.WriteLine("Eggs received by duckbear: " + eggsReceivedByDuckbear);

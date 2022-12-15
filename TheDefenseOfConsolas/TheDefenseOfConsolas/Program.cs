026cd0fd359d4da0492fe53ef6d3d5868193e6b3
James Thach <j.thach000@gmail.com>
2022-12-14 21:52:33 -0500

    Add background color for deployment instructions and logically space out program components

--- TheDefenseOfConsolas/TheDefenseOfConsolas/Program.cs
+++ TheDefenseOfConsolas/TheDefenseOfConsolas/Program.cs
@@ -1,17 +1,21 @@
 ﻿/* TODO:
  * O Ask user for target row
  * O Ask user for target column
- * - Compute the neighboring rows and columns
- * - Display deployment instructions in a different color
+ * O Compute the neighboring rows and columns
+ * O Display deployment instructions in a different color
  * O Change window title to be "Defense of Consolas"
  * O Play a sound when results have been computed and displayed
  */
 
 Console.Title = "Defense of Consolas";
+
 Console.Write("Target Row? ");
 int targetRow = Convert.ToInt16(Console.ReadLine()); 
 Console.Write("Target Column? ");
 int targetColumn = Convert.ToInt16(Console.ReadLine());
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+
 Console.WriteLine("Deploy to:");
 int northSquadmateRow = targetRow + 1;
 int northSquadmateColumn = targetColumn;
@@ -25,4 +29,5 @@ Console.WriteLine($"West Squadmate : ({westSquadmateRow}, {westSquadmateColumn})
 int eastSquadmateRow = targetRow;
 int eastSquadmateColumn = targetColumn + 1;
 Console.WriteLine($"East Squadmate : ({eastSquadmateRow}, {eastSquadmateColumn})");
+
 Console.Beep();

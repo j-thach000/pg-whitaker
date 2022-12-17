35c703cf14aecfffb023787187858d2f1f54f79e
James Thach <j.thach000@gmail.com>
2022-12-17 11:01:54 -0500

    Update TODO list by checking all items

--- DiscountedInventory/DiscountedInventory/Program.cs
+++ DiscountedInventory/DiscountedInventory/Program.cs
@@ -2,8 +2,8 @@
  * [x] Print the menu shown in the text
  * [x] Ask user to enter a number from the menu
  * [x] Use a switch statement or expression to output the menu item's cost
- * [] Modify program to ask user for name after inputting menu choice
- * [] If the inputted name is the same as the user's IRL name, halve costs
+ * [x] Modify program to ask user for name after inputting menu choice
+ * [x] If the inputted name is the same as the user's IRL name, halve costs
  */
 
 /* Prices (gold):

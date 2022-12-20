25023145d91d2c14a5ea17b96fd8255a55c2f7fc
James Thach <j.thach000@gmail.com>
2022-12-19 21:29:38 -0500

    Add completed challenge

---
+++ TakingANumber/TakingANumber/Program.cs
@@ -0,0 +1,40 @@
+﻿/* TODO: 
+ * [x] define a method AskForNumber 
+ * [x] prompt user for input
+ * [x] convert the string to int
+ * [x] return the output
+ * [x] define a method AskForNumberInRange
+ * [x] only have it return if the entered number is between min and max 
+ */
+
+int AskForNumber(string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    // completely valid as well, we just need a value in a certain range
+    int number;
+    do
+    {
+        Console.Write(text);
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    while (number < min || number > max);
+
+    return number;
+
+    /* since this code is in a function it's okay to set the loop this way
+     * as the value returned immediately ends the loop
+    while (true)
+    {
+        Console.Write(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= min || number <= max) return number;
+    }
+     */
+}

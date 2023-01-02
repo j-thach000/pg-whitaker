5c3fe9ebbb61ad7cfc68621156a887c970fd4d67
James Thach <j.thach000@gmail.com>
2023-01-02 02:38:27 -0500

    Edited to play around with the concept of recursion

--- Countdown/Countdown/Program.cs
+++ Countdown/Countdown/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// first attempt
+// recursive countdown print
+/*
+int Countdown(int value)
+{
+    Console.WriteLine(value);
+    if (value == 1) return 1; // close, but just stop on a value that's not
+                              // printed
+                              // also we can return in void methods to stop
+                              // early
+    return Countdown(value - 1);
+}
 
+Countdown(10);
+*/
+
+void Countdown(int value)
+{
+    // list base case first
+    if (value == 0) return;
+
+    Console.WriteLine(value);
+
+    // working towards the base case to end 
+    Countdown(value - 1);
+}
+
+Countdown(10);
